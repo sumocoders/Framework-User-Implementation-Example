c6c36073d954978237f8f39c8c2126a846645f42
Sander De la Marche <sander@sumocoders.be>
2021-05-11 22:33:44 +0200

    Fix codestyles

--- src/Message/User/ChangePassword.php
+++ src/Message/User/ChangePassword.php
@@ -7,20 +7,20 @@ use Symfony\Component\Validator\Constraints as Assert;
 
 class ChangePassword
 {
-  private User $user;
+    private User $user;
 
   /**
    * @Assert\NotBlank()
    */
-  public string $password;
+    public string $password;
 
-  public function __construct(User $user)
-  {
-    $this->user = $user;
-  }
+    public function __construct(User $user)
+    {
+        $this->user = $user;
+    }
 
-  public function getUser(): User
-  {
-    return $this->user;
-  }
+    public function getUser(): User
+    {
+        return $this->user;
+    }
 }
