e7226af1c6a510c2c2ff074800e525c687a9802c
Jonas De Keukelaere <jonas@sumocoders.be>
2024-10-09 13:52:43 +0100

    Add forgot passward to backend

--- src/Controller/User/Admin/EditUserController.php
+++ src/Controller/User/Admin/EditUserController.php
@@ -4,9 +4,11 @@ namespace App\Controller\User\Admin;
 
 use App\Entity\User\User;
 use App\Form\User\Admin\UserType;
+use App\Message\User\SendPasswordReset;
 use App\Message\User\UpdateUser;
 use SumoCoders\FrameworkCoreBundle\Attribute\Breadcrumb;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\FormType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Messenger\MessageBusInterface;
@@ -38,9 +40,26 @@ class EditUserController extends AbstractController
             return $this->redirectToRoute('user_overview');
         }
 
+        $sendPasswordResetMessage = new SendPasswordReset();
+        $sendPasswordResetMessage->email = $user->getEmail();
+        $passwordForgotForm = $this->createForm(FormType::class, $sendPasswordResetMessage);
+        $passwordForgotForm->handleRequest($request);
+
+        if ($passwordForgotForm->isSubmitted() && $passwordForgotForm->isValid()) {
+            $bus->dispatch($sendPasswordResetMessage);
+
+            $this->addFlash(
+                'success',
+                $translator->trans('Password reset successfully sent.')
+            );
+
+            return $this->redirectToRoute('user_edit', ['user' => $user->getId()]);
+        }
+
         return $this->render('user/admin/edit.html.twig', [
             'user' => $user,
-            'form' => $form->createView(),
+            'form' => $form,
+            'passwordForgotForm' => $passwordForgotForm,
         ]);
     }
 }
