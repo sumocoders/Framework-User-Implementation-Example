2edc60d15747d13cc80515a82f85fad20223e060
Jonas De Keukelaere <jonas@sumocoders.be>
2024-11-20 09:14:16 +0000

    Stop user from disableing own account

--- src/Controller/User/Admin/DisableUserController.php
+++ src/Controller/User/Admin/DisableUserController.php
@@ -10,6 +10,9 @@ use Symfony\Component\Messenger\MessageBusInterface;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Contracts\Translation\TranslatorInterface;
 
+/**
+ * @method User getUser()
+ */
 class DisableUserController extends AbstractController
 {
     #[Route('/admin/users/{user}/disable', name: 'user_disable')]
@@ -18,6 +21,10 @@ class DisableUserController extends AbstractController
         TranslatorInterface $translator,
         MessageBusInterface $bus
     ): Response {
+        if ($user->getId() === $this->getUser()->getId()) {
+            throw $this->createAccessDeniedException();
+        }
+
         $bus->dispatch(new DisableUser($user));
 
         $this->addFlash(
