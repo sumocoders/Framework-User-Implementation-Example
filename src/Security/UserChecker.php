192665f7f165ba0bf2666adc198147901fca6a32
Tijs Verkoyen <tijs@verkoyen.eu>
2025-04-17 09:28:13 +0200

    Fix translations. use {XXX} instead of %XXX$ for variables

--- src/Security/UserChecker.php
+++ src/Security/UserChecker.php
@@ -30,9 +30,9 @@ final class UserChecker implements UserCheckerInterface
         if (!$user->isConfirmed()) {
             throw new UnconfirmedAccountException(
                 $this->translator->trans(
-                    'Account has not been confirmed. <a href="%resendConfirmationUrl%">Resend confirmation mail</a>',
+                    'Account has not been confirmed. <a href="{resendConfirmationUrl}">Resend confirmation mail</a>',
                     [
-                        '%resendConfirmationUrl%' => $this->router->generate(
+                        'resendConfirmationUrl' => $this->router->generate(
                             'resend_confirmation',
                             [
                                 'token' => $user->getConfirmationToken(),
