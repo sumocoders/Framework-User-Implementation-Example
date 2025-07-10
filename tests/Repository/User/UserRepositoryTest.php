c644f58f84dd35ece7e6647fae51f74a9d34e598
Tijs Verkoyen <tijs@verkoyen.eu>
2025-07-10 16:20:25 +0200

    Improved (added) tests

---
+++ tests/Repository/User/UserRepositoryTest.php
@@ -0,0 +1,137 @@
+<?php
+
+namespace App\Tests\Repository\User;
+
+use App\DataTransferObject\User\FilterDataTransferObject;
+use App\Entity\User\User;
+use App\Repository\User\UserRepository;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\Tools\SchemaTool;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+use Symfony\Component\HttpKernel\KernelInterface;
+use Symfony\Component\Security\Core\Exception\UnsupportedUserException;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+
+class UserRepositoryTest extends KernelTestCase
+{
+    private UserRepository $userRepository;
+
+    protected function setUp(): void
+    {
+        $kernel = self::bootKernel();
+        $this->initDatabase($kernel);
+        $this->userRepository = $kernel->getContainer()
+            ->get('doctrine')
+            ->getManager()
+            ->getRepository(User::class);
+    }
+
+    public function testAddUserWithRoleAdmin(): void
+    {
+        $newUser = new User('user@example.com', ['ROLE_ADMIN']);
+        $this->userRepository->add($newUser);
+        $user = $this->userRepository->findOneBy(['email' => 'user@example.com']);
+
+        $this->assertInstanceOf(User::class, $user);
+        $this->assertSame(1, $user->getId());
+        $this->assertSame('user@example.com', $user->getEmail());
+        $this->assertContains('ROLE_ADMIN', $user->getRoles());
+    }
+
+    public function testUpgradePasswordWithUserInstance(): void
+    {
+        $newUser = new User('user@example.com', ['ROLE_ADMIN']);
+        $this->userRepository->upgradePassword($newUser, 'super-secret-password');
+        $this->assertSame($newUser->getPassword(), 'super-secret-password');
+    }
+
+    public function testUpgradePasswordWithInvalidUserInstance(): void
+    {
+        $newUser = $this->createMock(PasswordAuthenticatedUserInterface::class); // Not a User instance
+        $this->expectException(UnsupportedUserException::class);
+        $this->userRepository->upgradePassword($newUser, 'super-secret-password');
+    }
+
+    public function testCheckConfirmationTokenWithValidToken(): void
+    {
+        $newUser = new User('user@example.com', ['ROLE_ADMIN']);
+        $this->userRepository->add($newUser);
+        $newUser->requestConfirmation();
+        $this->userRepository->save();
+        $user = $this->userRepository->checkConfirmationToken($newUser->getConfirmationToken());
+
+        $this->assertInstanceOf(User::class, $user);
+        $this->assertSame('user@example.com', $user->getEmail());
+    }
+
+    public function testCheckConfirmationTokenWithInvalidToken(): void
+    {
+        $user = $this->userRepository->checkConfirmationToken('non-existing-token');
+
+        $this->assertNull($user);
+    }
+
+    public function testCheckResetTokenWithValidToken(): void
+    {
+        $newUser = new User('user@example.com', ['ROLE_ADMIN']);
+        $this->userRepository->add($newUser);
+        $newUser->requestPassword();
+        $this->userRepository->save();
+        $user = $this->userRepository->checkResetToken($newUser->getPasswordResetToken());
+
+        $this->assertInstanceOf(User::class, $user);
+        $this->assertSame('user@example.com', $user->getEmail());
+        $this->assertEqualsWithDelta($user->getPasswordRequestedAt(), new \DateTimeImmutable(), 1);
+    }
+
+    public function testCheckResetTokenWithInvalidToken(): void
+    {
+        $user = $this->userRepository->checkResetToken('non-existing-token');
+
+        $this->assertNull($user);
+    }
+
+    public function testFilterForExistingUser(): void
+    {
+        $newUser = new User('user@example.com', ['ROLE_ADMIN']);
+        $this->userRepository->add($newUser);
+        $otherUser = new User('user2@example.com', ['ROLE_ADMIN']);
+        $this->userRepository->add($otherUser);
+
+        $userFilter = new FilterDataTransferObject();
+        $userFilter->term = 'user@example.com';
+        $paginator = $this->userRepository->getAllFilteredUsers($userFilter);
+        $paginator->paginate();
+
+        $this->assertSame(1, $paginator->count());
+        $this->assertInstanceOf(User::class, $paginator->getResults()[0]);
+        $this->assertSame('user@example.com', $paginator->getResults()[0]->getEmail());
+    }
+
+    public function testFilterWithoutTerm(): void
+    {
+        $newUser = new User('user@example.com', ['ROLE_ADMIN']);
+        $this->userRepository->add($newUser);
+        $otherUser = new User('other-user@example.com', ['ROLE_ADMIN']);
+        $this->userRepository->add($otherUser);
+
+        $userFilter = new FilterDataTransferObject();
+        $paginator = $this->userRepository->getAllFilteredUsers($userFilter);
+        $paginator->paginate();
+
+        $this->assertSame(2, $paginator->count());
+        $this->assertInstanceOf(User::class, $paginator->getResults()[0]);
+        $this->assertSame('user@example.com', $paginator->getResults()[0]->getEmail());
+        $this->assertInstanceOf(User::class, $paginator->getResults()[1]);
+        $this->assertSame('other-user@example.com', $paginator->getResults()[1]->getEmail());
+    }
+
+    private function initDatabase(KernelInterface $kernel): void
+    {
+        /** @var EntityManager $entityManager */
+        $entityManager = $kernel->getContainer()->get('doctrine.orm.entity_manager');
+        $metaData = $entityManager->getMetadataFactory()->getAllMetadata();
+        $schemaTool = new SchemaTool($entityManager);
+        $schemaTool->updateSchema($metaData);
+    }
+}
---
+++ tests/Security/UserCheckerTest.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Tests\Security;
+
+use App\Entity\User\User;
+use App\Repository\User\UserRepository;
+use App\Security\Exception\UnconfirmedAccountException;
+use App\Security\UserChecker;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\Routing\RouterInterface;
+use Symfony\Component\Security\Core\Exception\DisabledException;
+use Symfony\Contracts\Translation\TranslatorInterface;
+
+class UserCheckerTest extends TestCase
+{
+    private UserChecker $userChecker;
+
+    protected function setUp(): void
+    {
+        $this->userChecker = new UserChecker(
+            $this->createMock(TranslatorInterface::class),
+            $this->createMock(RouterInterface::class),
+            $this->createMock(UserRepository::class)
+        );
+    }
+
+    public function testUnconfirmedUserThrowsException(): void
+    {
+        $user = new User('user@example.com', ['ROLE_USER']);
+
+        $this->expectException(UnconfirmedAccountException::class);
+
+        $this->userChecker->checkPreAuth($user);
+    }
+
+    public function testDisabledUserThrowsException(): void
+    {
+        $user = new User('user@example.com', ['ROLE_USER']);
+        $user->confirm();
+        $user->disable();
+
+        $this->expectException(DisabledException::class);
+
+        $this->userChecker->checkPreAuth($user);
+    }
+
+    public function testPostAuthErasesPasswordResetToken(): void
+    {
+        $user = new User('user@example.com', ['ROLE_USER']);
+        $user->requestPassword();
+
+        $this->userChecker->checkPostAuth($user);
+
+        $this->assertNull($user->getPasswordResetToken());
+        $this->assertNull($user->getPasswordRequestedAt());
+    }
+}
