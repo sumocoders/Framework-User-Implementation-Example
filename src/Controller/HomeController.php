39ae1cf6f7794c00f00b574e94ecc7ffa532950b
Abraham <abraham@sumocoders.be>
2025-08-05 12:16:02 +0200

    Redirect to preferred language

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,16 +2,19 @@
 
 namespace App\Controller;
 
-use App\Controller\User\ProfileController;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\DependencyInjection\Attribute\Autowire;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'home')]
-    public function __invoke(): Response
-    {
-        return $this->redirectToRoute('user_profile');
+    public function __invoke(
+        Request $request,
+        #[Autowire('%locales%')] array $locales,
+    ): Response {
+        return $this->redirectToRoute('user_profile', ['_locale' => $request->getPreferredLanguage($locales)]);
     }
 }
