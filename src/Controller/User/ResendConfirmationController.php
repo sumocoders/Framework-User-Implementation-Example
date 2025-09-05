d76192e76eaff398496b3529abc6d8ca847eb37b
Tijs Verkoyen <tijs@verkoyen.eu>
2025-09-05 10:15:41 +0200

    fix: forgotten use statement

--- src/Controller/User/ResendConfirmationController.php
+++ src/Controller/User/ResendConfirmationController.php
@@ -6,6 +6,7 @@ use App\Entity\User\User;
 use App\Message\User\SendConfirmation;
 use App\Repository\User\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Messenger\MessageBusInterface;
 use Symfony\Component\Routing\Attribute\Route;
