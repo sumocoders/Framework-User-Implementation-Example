17029503ddb40b86d945b4bc9616178c01156ea1
Ilaria Orlando <ilaria@sumocoders.be>
2026-01-08 15:02:30 +0100

    refactor to match RegisterUser

--- src/Message/User/ResetPassword.php
+++ src/Message/User/ResetPassword.php
@@ -7,7 +7,9 @@ use Symfony\Component\Validator\Constraints as Assert;
 
 class ResetPassword
 {
-    #[Assert\PasswordStrength(minScore: Assert\PasswordStrength::STRENGTH_STRONG)]
+    #[Assert\PasswordStrength(
+        minScore: Assert\PasswordStrength::STRENGTH_STRONG
+    )]
     #[Assert\NotCompromisedPassword()]
     #[Assert\Length(min: 12)]
     public string $password;
