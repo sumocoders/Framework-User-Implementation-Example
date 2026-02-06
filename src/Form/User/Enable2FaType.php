d53a453148961ac61e45b39d97a5fe527458c9fc
Tijs Verkoyen <tijs@verkoyen.eu>
2026-02-06 17:46:43 +0100

    fix: use text type for OTP code

--- src/Form/User/Enable2FaType.php
+++ src/Form/User/Enable2FaType.php
@@ -4,7 +4,7 @@ namespace App\Form\User;
 
 use App\Message\User\Enable2Fa;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -17,10 +17,14 @@ class Enable2FaType extends AbstractType
     {
         $builder->add(
             'code',
-            NumberType::class,
+            TextType::class,
             [
                 'label' => 'Code',
                 'attr' => [
+                    'autocomplete' => 'one-time-code',
+                    'autofocus' => true,
+                    'inputmode' => 'numeric',
+                    'pattern' => '[0-9]*',
                     'placeholder' => 'Enter the code from your authenticator app',
                 ],
             ]
