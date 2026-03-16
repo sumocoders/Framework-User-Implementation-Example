57a55cf3e8dc0686e5190655ad6ec469d42c81b0
Ilias Khatsjiyev <ilias@sumocoders.be>
2026-03-16 12:04:44 +0100

    fix: move change email into seperate controller

--- src/Controller/User/ProfileController.php
+++ src/Controller/User/ProfileController.php
@@ -8,45 +8,16 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Attribute\CurrentUser;
-use Symfony\Contracts\Translation\TranslatorInterface;
-use App\Message\User\ChangeEmail;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Messenger\MessageBusInterface;
-use App\Form\User\ChangeEmailType;
 
 #[Route('/user/profile', name: 'user_profile')]
 class ProfileController extends AbstractController
 {
-    public function __construct(
-        private readonly TranslatorInterface $translator,
-        private readonly MessageBusInterface $messageBus,
-    ) {
-    }
-
-    #[Breadcrumb('Email')]
+    #[Breadcrumb('Profile')]
     public function __invoke(
-        Request $request,
         #[CurrentUser] User $user
     ): Response {
-        $message = new ChangeEmail($user->getId(), $user->getEmail());
-
-        $form = $this->createForm(ChangeEmailType::class, $message);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $this->messageBus->dispatch($form->getData());
-
-            $this->addFlash(
-                'success',
-                $this->translator->trans('Email successfully edited.')
-            );
-
-            return $this->redirectToRoute('user_profile');
-        }
-
-        return $this->render('user/profile/email.html.twig', [
+        return $this->render('user/profile/index.html.twig', [
             'user' => $user,
-            'form' => $form,
         ]);
     }
 }
